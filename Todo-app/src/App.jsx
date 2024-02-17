e1362071b7fe8abef85353df914d1ee7847bbd49
pirate776 <kingzaljaguar@gmail.com>
2024-02-17 14:34:15 +0300

    styling added

--- Todo-app/src/App.jsx
+++ Todo-app/src/App.jsx
@@ -1,10 +1,14 @@
  import Todo from './Components/Todo'
+import Header from './Components/Header'
+import './App.css';
 function App() {
    
 
   return (
     <div className="App">
+      <Header />
       <Todo />
+      
     </div>
   )
 }
---
+++ Todo-app/src/Components/Header.jsx
@@ -0,0 +1,4 @@
+import styles from './header.module.css'
+export default function Header(){
+    return <div className={styles.header}>My todo list</div>
+}
